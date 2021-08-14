085caf97f4ceee470c240369e413f3e9c25931c6
overflowerror <mail@overflowerror.com>
2021-08-14 15:04:51 +0200

    mysql doesn't support uuid as a type

--- backend/internal/data/models/base.go
+++ backend/internal/data/models/base.go
@@ -7,7 +7,7 @@ import (
 )
 
 type BaseModel struct {
-	ID        uuid.UUID `gorm:"type:uuid;primary_key;"`
+	ID        uuid.UUID `gorm:"type:char(36);primary_key;"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
